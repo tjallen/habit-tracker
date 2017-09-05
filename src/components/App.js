7fa37d290e43aa068d235337bb17354b2bd49bfa
Thom Allen <thomwork@gmail.com>
2017-09-05 13:22:48 +0100

    Move App to new components directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { observer } from 'mobx-react';
-import taskStore from './stores/taskStore';
+import taskStore from '../stores/taskStore';
 
 @observer
 class App extends Component {
