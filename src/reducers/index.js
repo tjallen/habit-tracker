c7f5aa85a3854589bbe52c47fd6de84e5686b00c
Thom Allen <thomwork@gmail.com>
2017-09-15 18:23:59 +0100

    Delete unused placeholder settings reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import tasks from './tasks';
-import settings from './settings';
 import date from './date';
 
 const rootReducer = combineReducers({
   tasks,
-  settings,
   date,
 });
 
